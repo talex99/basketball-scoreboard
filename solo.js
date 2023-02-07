9b62658e9d225a3736724c65635b4b826401c644
talex99 <124080244+talex99@users.noreply.github.com>
2023-02-07 16:06:11 +0100

    Add files via upload

---
+++ solo.js
@@ -0,0 +1,32 @@
+
+function incrementHome(){
+    document.getElementById("br1").textContent++;
+}
+
+function incrementGuest(){
+    document.getElementById("br2").textContent++;
+}
+
+function Homeplus2(){
+    incrementHome();
+    incrementHome();
+}
+
+function Homeplus3(){
+    incrementHome();
+    incrementHome();
+    incrementHome();  
+}
+
+function Guestplus2(){
+    incrementGuest()
+    incrementGuest()
+}
+
+function Guestplus3(){
+    incrementGuest()
+    incrementGuest()
+    incrementGuest() 
+}
+
+
